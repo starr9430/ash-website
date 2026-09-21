efbdde3df0341195ea9cd4c74c7d2c3e8f0ede99
starr9430 <starr@ashlira.com>
2026-09-21 14:38:01 +0800

    feat: establish ASH visual system sections

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="site-footer">
+      <span>ASH / GLOBAL CLUTCH SOLUTIONS</span>
+      <span>© 2026 ASH</span>
+    </footer>
+  )
+}
+
+export default Footer
