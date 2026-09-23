49e6d98f5c23374a8011efdcef1ceff5932866c3
starr9430 <starr@ashlira.com>
2026-09-23 11:59:56 +0800

    Update ASH contact email

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
         <a href="#top">Home</a><i /> <a href="#products">Products</a><i /> <a href="#products">About Us</a><i /> <a href="#contact">Contact</a>
       </nav>
       <div className="footer-contact">
-        <a href="mailto:hello@ashlira.com">✉ &nbsp; hello@ashlira.com</a>
+        <a href="mailto:starr@ashlira.com">✉ &nbsp; starr@ashlira.com</a>
         <span>© 2026 ASH. All rights reserved.</span>
       </div>
       <img className="footer-leaf" src="/images/brand/footer-leaf.svg" alt="" />
