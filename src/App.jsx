4a518c12630a9042c2c8572ed0d016723bebdc84
starr9430 <starr@ashlira.com>
2026-09-25 11:26:16 +0800

    Fix About Us navigation anchor

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Header from './components/Header'
 import Hero from './sections/Hero'
 import FeatureStrip from './sections/FeatureStrip'
 import Coverage from './sections/Coverage'
-import Intro from './sections/Intro'
 import Contact from './sections/Contact'
 import Footer from './components/Footer'
 
@@ -14,7 +13,6 @@ function App() {
         <Hero />
         <FeatureStrip />
         <Coverage />
-        <Intro />
         <Contact />
       </main>
       <Footer />
